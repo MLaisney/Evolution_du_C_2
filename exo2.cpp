4e463c9e8e80718bec999edc26987d528a332def
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-22 14:30:13 +0200

    Create program 2

---
+++ exo2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    for (int n=10; n>0; n--)
+        cout << n << ",";
+    cout << "GO!" << endl;
+    return 0;
+}
