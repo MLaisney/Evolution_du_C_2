96cdb84044d3aca66ff2f1d146ed55fecccc8711
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-22 15:00:42 +0200

    Create program 6

---
+++ exo6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    string const nomFichier("C:\c\scores.txt");
+    ofstream monFlux(nomFichier.c_str());
+    if (monFlux)
+    {
+        cout << "Pas de souci lors de l'ouverture du fichier." << endl;
+    }
+    else
+    {
+        cout << "ERREUR : Impossible d'ouvrir le fichier." << endl;
+    }
+    return 0;
+}
