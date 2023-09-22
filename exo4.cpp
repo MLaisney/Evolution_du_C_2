dfeede3cb830a4c2d03f8179ed414041c456df75
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-22 14:53:32 +0200

    Create program 4

--- exo4.cpp
+++ exo4.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
-int sum (int a, int b )
-{
-    return (a+b);
-}
-double sum (double a, double b)
-{
-    return (a+b);
-}
 int main()
 {
-    cout << sum(10,20) << endl;
-    cout << sum(1.0,1.5) << endl;
+    ofstream monFlux("C:\c\scores.txt");
     return 0;
 }
