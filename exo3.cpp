28869f43539b3c7d7543ad4defa16d38854a756a
MLaisney <laisney.melvyn@lyceejulesfil.fr>
2023-09-22 14:36:48 +0200

    Create program 3

---
+++ exo3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str ("Coucou les copains !");
+    for (int i(0); i<21; i++)
+    {
+        cout << str[i] << '';
+    }
+    cout << endl;
+    return 0;
+}
+
+// à revoir
